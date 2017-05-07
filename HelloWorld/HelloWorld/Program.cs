63c0ea066607bfa61796e6214c8991092370f27c
Rafał Markowicz <rafal.markowicz@codesprinters.com>
2017-05-07 10:04:31 +0200

    Zadanie 1: HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj świecie!");
+            Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyc...");
+            Console.ReadKey();
+        }
+    }
+}
