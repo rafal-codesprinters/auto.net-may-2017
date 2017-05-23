c0b2caf3700447f83c5de84ba65e1cdc4e39e424
Arkadiusz Benedykt <abenedykt@rekord.com.pl>
2017-05-23 15:33:01 +0200

    added tests for calc app

---
+++ Cwiczenie1/Cwiczenie1.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cwiczenie1.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // arrange
+            var calculator = new Calc(20,15);
+
+            // act
+            var result = calculator.Add();
+
+            //assert
+            if(result == 35)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("OK");
+            } else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Blad");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
