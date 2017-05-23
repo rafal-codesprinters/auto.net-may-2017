72791a7ca2b30cf0d48efb43b961f41e77b690ea
Arkadiusz Benedykt <abenedykt@rekord.com.pl>
2017-05-23 17:29:41 +0200

    added first selenium test

---
+++ Cwiczenie1/GoogleTesting/GoogleTest.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace GoogleTesting
+{
+    public class GoogleTest : IDisposable
+    {
+        private ChromeDriver _driver;
+
+        public GoogleTest()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage()
+                .Timeouts()
+                .ImplicitlyWait(TimeSpan.FromSeconds(5));
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+        }
+
+        [Fact]
+        public void Hello_test()
+        {
+            _driver.Navigate().GoToUrl("http://www.google.com");
+            _driver.FindElementById("lst-ib").SendKeys("code sprinters");
+            _driver.FindElementById("_fZl").Click();
+            var result = _driver.FindElementByXPath(@"//div/*/a[@href='http://agileszkolenia.pl/']");
+
+            Assert.NotNull(result);
+            Assert.Equal("Code Sprinters -", result.Text);
+        }
+
+
+    }
+}
