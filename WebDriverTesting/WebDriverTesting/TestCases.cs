eb6e450b37b9540db370e060423c9346e2700e54
Arkadiusz Benedykt <abenedykt@rekord.com.pl>
2017-05-24 10:59:23 +0200

    added first test for the second note

--- WebDriverTesting/WebDriverTesting/TestCases.cs
+++ WebDriverTesting/WebDriverTesting/TestCases.cs
@@ -1,13 +1,13 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
+using System.Collections.ObjectModel;
 using Xunit;
 
 namespace WebDriverTesting
 {
     public class TestCases : IDisposable
     {
-        private ChromeDriver _driver;
-
         public TestCases()
         {
             _driver = new ChromeDriver();
@@ -17,14 +17,22 @@ namespace WebDriverTesting
         }
 
         [Fact]
-        public void FixMe()
+        public void Second_note_should_have_title_Vivamus_aliquam_feugiat()
         {
-            Assert.Equal(true, false);
+            _driver.Navigate().GoToUrl("https://autotestdotnet.wordpress.com/");
+
+            ReadOnlyCollection<IWebElement> posts = _driver.FindElementsByClassName("post-title");
+            IWebElement secondPost = posts[1];
+            string firstNoteTitle = secondPost.FindElement(By.TagName("a")).Text;
+            
+            Assert.Equal("Vivamus aliquam feugiat", firstNoteTitle);
         }
 
         public void Dispose()
         {
             _driver.Quit();
         }
+
+        private ChromeDriver _driver;
     }
 }
