0af40fc9dc0a3c78ecdedaae4d36c29badb860af
Arkadiusz Benedykt <abenedykt@rekord.com.pl>
2017-05-25 10:17:51 +0200

    cleaning project

---
+++ WebDriverTesting/WebDriverTesting/BlogPostTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace WebDriverTesting
+{
+    public class BlogPostTests
+    {
+        [Fact]
+        public void When_user_is_not_logged_in_can_add_comment_on_second_note()
+        {
+
+        }
+    }
+}
