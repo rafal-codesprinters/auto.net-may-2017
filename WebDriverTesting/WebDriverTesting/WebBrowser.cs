0a77dc57a5d1acffc13b26f697f6d9f4310e8a3e
Arkadiusz Benedykt <abenedykt@rekord.com.pl>
2017-05-25 16:44:22 +0200

    going back to chrome

--- WebDriverTesting/WebDriverTesting/WebBrowser.cs
+++ WebDriverTesting/WebDriverTesting/WebBrowser.cs
@@ -10,7 +10,7 @@ namespace WebDriverTesting
     {
         static WebBrowser()
         {
-            Driver = new FirefoxDriver();
+            Driver = new ChromeDriver();
             Driver.Manage()
                 .Window
                 .Size = new System.Drawing.Size(Configuration.BrowserWidth, 
